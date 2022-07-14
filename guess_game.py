a05272936013834ad9b556b4021cba09a342d095
jordaj2 <joel.jordan@weyerhaeuser.com>
2022-07-13 22:01:23 -0700

    Update guess_game.py

--- guess_game.py
+++ guess_game.py
@@ -1,31 +1,29 @@
-# Have the comoputer generate a random #
+# Have the computer generate a random #
 # Have the user enter a number
 # if the number is correct end the program and congratulate the user
 # if the number is not correct repeat asking the user for the number
 import random
 
 
-def ask_user(number:int):
-    user_guess = ""
+def ask_user(number: int):
     user_guess = input("Guess the number")
-    user_guess = int(user_guess)
-    
-    if number == user_guess:
+    int_user_guess = int(user_guess)
+
+    if number == int_user_guess:
         return True
     else:
-        if user_guess > number:
+        if int_user_guess > number:
             print("You are way to high!! Guess a lower number")
-        if user_guess < number:
-            if (user_guess/number * 100) > 75:
-                print("You are getting hot")
-            if (user_guess/number) * 100 > 50 and (user_guess/number) * 100 < 75:
-                print("You are warm")
-            if (user_guess/number) * 100 < 50 and (user_guess/number) * 100 > 15:
-                print("You are cold")
-            if (user_guess/number) * 100 < 15:
-                print("You are freezing")
+        if int_user_guess < number:
+            if (int_user_guess / number * 100) > 75:
+                print("You are getting hot.\n")
+            if (int_user_guess / number) * 100 > 50 and (int_user_guess / number) * 100 < 75:
+                print("You are warm.\n")
+            if (int_user_guess / number) * 100 < 50 and (int_user_guess / number) * 100 > 15:
+                print("You are cold.\n")
+            if (int_user_guess / number) * 100 < 15:
+                print("You are freezing.\n")
         return False
-
 
 
 def guess_game(num):
@@ -33,7 +31,7 @@ def guess_game(num):
         is_correct = False
         while not is_correct:
             is_correct = ask_user(num)
-        print(f"Congratulations you guessed the number")
+        print(f"Congratulations you guessed the number.\n")
     except Exception as e:
         print(f"Error {e}")
 
@@ -41,7 +39,5 @@ def guess_game(num):
 # Generate a random number between 1 and 1000
 random_number = random.randint(1, 1000)
 
-
 # Start the game
 guess_game(random_number)
-
